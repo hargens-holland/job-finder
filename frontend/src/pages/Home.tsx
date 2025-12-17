720730a6c9d16d67285a7a7ac06359a12f8b2085
hargens-holland <hshargens@verizon.net>
2025-12-17 15:54:00 -0500

    Frontend framework up and running

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold text-blue-600">
+                Job Finder Application Tracking System
+            </h1>
+        </div>
+    );
+}
